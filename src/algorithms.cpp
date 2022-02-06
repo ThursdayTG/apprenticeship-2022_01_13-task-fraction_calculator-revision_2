df9bd0736a5b9f6b4ebf4489373c82ea253eabbd
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2022-02-07 00:28:07 +0100

    Update algorithms.cpp

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -1,21 +1,24 @@
-int cancellingNum(int numerator, int denominator) {
-	for	(int i = 2;    i <= numerator + denominator;    i++) {
-		if (numerator % i == 0) {
-			if (denominator % i == 0) {
-				return numerator / i;
-			}
+int cancellingNum(int numerator, int denominator)
+{
+	for	(int i = 2; i <= numerator + denominator; i++)
+	{
+		if (numerator % i == 0 && denominator % i == 0)
+		{
+			return numerator / i;
 		}
 	}
 
 	return numerator;
 }
 
-int cancellingDen(int numerator, int denominator) {
-	for	(int i = 2;    i <= numerator + denominator;    i++) {
-		if (numerator % i == 0) {
-			if (denominator % i == 0) {
-				return denominator / i;
-			}
+
+int cancellingDen(int numerator, int denominator)
+{
+	for	(int i = 2; i <= numerator + denominator; i++)
+	{
+		if (numerator % i == 0 && denominator % i == 0)
+		{
+			return denominator / i;
 		}
 	}
 
@@ -24,37 +27,40 @@ int cancellingDen(int numerator, int denominator) {
 
 
 
-float A(float inputNumerator1, float inputDenominator1, float inputNumerator2, float inputDenominator2) {
 
-	float	fraction1 = inputNumerator1 / inputDenominator1;
-	float	fraction2 = inputNumerator2 / inputDenominator2;
+float addition(float inNum1, float inDen1, float inNum2, float inDen2) {
+
+	float	fraction1 = inNum1 / inDen1;
+	float	fraction2 = inNum2 / inDen2;
 	float	result = fraction1 + fraction2;
 
 	return result;
 }
 
-float B(float inputNumerator1, float inputDenominator1, float inputNumerator2, float inputDenominator2) {
 
-	float	fraction1 = inputNumerator1 / inputDenominator1;
-	float	fraction2 = inputNumerator2 / inputDenominator2;
+float subtraction(float inNum1, float inDen1, float inNum2, float inDen2) {
+
+	float	fraction1 = inNum1 / inDen1;
+	float	fraction2 = inNum2 / inDen2;
 	float	result = fraction1 - fraction2;
 
 	return result;
 }
 
-float C(float inputNumerator1, float inputDenominator1, float inputNumerator2, float inputDenominator2) {
+float multiplication(float inNum1, float inDen1, float inNum2, float inDen2) {
 
-	float	fraction1 = inputNumerator1 / inputDenominator1;
-	float	fraction2 = inputNumerator2 / inputDenominator2;
+	float	fraction1 = inNum1 / inDen1;
+	float	fraction2 = inNum2 / inDen2;
 	float	result = fraction1 * fraction2;
 
 	return result;
 }
 
-float D(float inputNumerator1, float inputDenominator1, float inputNumerator2, float inputDenominator2) {
 
-	float	fraction1 = inputNumerator1 / inputDenominator1;
-	float	fraction2 = inputNumerator2 / inputDenominator2;
+float division(float inNum1, float inDen1, float inNum2, float inDen2) {
+
+	float	fraction1 = inNum1 / inDen1;
+	float	fraction2 = inNum2 / inDen2;
 	float	result = fraction1 / fraction2;
 
 	return result;
