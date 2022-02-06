45b0f95bb89aa441b9d725c281a1a1a977b5cad5
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2022-02-07 00:31:48 +0100

    Add files via upload

---
+++ headers/algorithms.hpp
@@ -0,0 +1,14 @@
+#ifndef HEADERS_ALGORITHMS
+#define HEADERS_ALGORITHMS
+
+
+int cancelNum(int, int);
+int cancelDen(int, int);
+
+float addition(float, float, float, float);
+float subtraction(float, float, float, float);
+float multiplication(float, float, float, float);
+float division(float, float, float, float);
+
+
+#endif
---
+++ headers/userInput.hpp
@@ -0,0 +1,9 @@
+#ifndef HEADERS_USERINPUT
+#define HEADERS_USERINPUT
+
+
+int inputInt();
+int inputChar();
+
+
+#endif
